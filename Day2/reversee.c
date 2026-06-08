ae98b5891251a079e0ed50c2adaf7b3a595e2a57
sakshi anand <sakshianand7545@gmail.com>
2026-06-08 19:25:17 +0530

    Day2

---
+++ Day2/reversee.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int num,digit,reverse = 0;
+
+    printf("enter a num:");
+    scanf("%d",&num);
+
+    while(num != 0){
+        digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = num / 10;
+    }
+    printf("Reversed number = %d\n",reverse);
+    return 0;
+
+}
